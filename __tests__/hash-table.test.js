be63b68beac1f3f6801fde54b52d05519904d776
Kim Robinson <kimmykokonut@gmail.com>
2024-03-06 20:11:55 -0800

    pass test for .set()

--- __tests__/hash-table.test.js
+++ __tests__/hash-table.test.js
@@ -16,4 +16,9 @@ describe('HashTable', () => {
     expect(hashTable.hash("zygorth")).toEqual(25);
   });
 
+  test('should correctly set a key-value pair in a hash table', () => {
+    hashTable.set("John", "Lead Singer");
+    expect(hashTable.array[9]).toEqual([["John", "Lead Singer"]]);
+  });
+
 });
