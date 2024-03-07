73763233f7970d0993d8849a3a286373f3c345c5
Kim Robinson <kimmykokonut@gmail.com>
2024-03-06 20:00:21 -0800

    pass test has letter to num

--- __tests__/hash-table.test.js
+++ __tests__/hash-table.test.js
@@ -10,5 +10,10 @@ describe('HashTable', () => {
   test('should instantiate a hash with an empty array', () => {
     expect(hashTable.array).toEqual([]);
   });
-  
+
+  test('should return a number representation of a letter', () => {
+    expect(hashTable.hash("Alaric")).toEqual(0);
+    expect(hashTable.hash("zygorth")).toEqual(25);
+  });
+
 });
--- src/hash-table.js
+++ src/hash-table.js
@@ -2,5 +2,8 @@ export default class HashTable {
   constructor() {
     this.array = [];
   }
+  hash(key) {
+    return key.charAt(0).toLowerCase().charCodeAt(0) - 97;
+  }
   
 }
